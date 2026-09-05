6490aa5b1d2175dd266f837a03045178172218b4
Ketelbinkie971 <roelmeirik@gmail.com>
2026-09-05 18:44:55 +0200

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "com.ketelcustoms.radarwallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 28
-        versionName = "0.28"
+        versionCode = 29
+        versionName = "0.29"
     }
 
     buildTypes {
